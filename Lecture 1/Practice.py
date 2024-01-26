4f05ad923f9086590a98669bcf576e79454bd60f
Simran <simrantailor1000@gmail.com>
2024-01-27 00:07:36 +0530

    Lecture 1 practice

---
+++ Lecture 1/Practice.py
@@ -0,0 +1,18 @@
+# 1. Write a Program to input 2 numbers & print their sum
+First = int(input("Enter First Number:"))
+Second = int(input("Enter Second Number:"))
+print(First + Second)
+
+# 2. Write a Program to input side of a square and print its area
+Side = float(input("Enter the side of square:"))
+print("Area:", Side * Side)
+
+# 3. Write a Program to input 2 floating point numbers and print their average
+First = float(input("Enter First Number:"))
+Second = float(input("Enter Second Number:"))
+print("Average:", (First + Second)/2)
+
+# 4. Write a Program to input 2 int numbers, a and b. Print True if a is greater than or equal to b. If not print False
+a = int(input("Enter the value:"))
+b = int(input("Enter the value:"))
+print(a >= b)
